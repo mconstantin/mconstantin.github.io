63a612ca29eadb51433b31538af056eb6ca23af3
Michael Constantin <constantinm@sharplabs.com>
2016-09-29 11:18:44 -0700

    added loading indicator

--- mod3-solution/js/app.js
+++ mod3-solution/js/app.js
@@ -5,21 +5,26 @@ angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
 .service('MenuSearchService', MenuSearchService)
 .directive('foundItems', FoundItemsDirective)
+.directive('itemsLoaderIndicator', ItemsLoaderIndicatorDirective)
 .constant('baseUrl', "https://davids-restaurant.herokuapp.com/");
 
 NarrowItDownController.$inject = ['$scope', 'MenuSearchService'];
 function NarrowItDownController($scope, MenuSearchService) {
   var ctrl = this;
   ctrl.search = "";
-  ctrl.found = [];
+  // ctrl.found = [];
   ctrl.show_empty_search = false;
+  ctrl.loading = false;
 
   $scope.$watch(function(scope) {
       return (ctrl.search);
     },
     function(newValue, oldValue) {
-      newValue = newValue || "";
-      ctrl.show_empty_search = oldValue && newValue.trim().length === 0;
+      // newValue = newValue || "";
+      // ctrl.show_empty_search = oldValue && newValue.trim().length === 0;
+      // reset empty search warning until user clicks the find button again
+      ctrl.show_empty_search = false;
+      ctrl.found = undefined;
   });
 
   ctrl.findMenuItems = function() {
@@ -27,15 +32,20 @@ function NarrowItDownController($scope, MenuSearchService) {
     ctrl.found = [];
     // nothing to search if search term is empty
     if (ctrl.search.trim().length === 0) {
+      ctrl.show_empty_search = true;
       return;
     }
 
     var promise = MenuSearchService.getMatchedMenuItems(ctrl.search);
+    ctrl.loading = true;
     promise.then(function(result) {
       ctrl.found = result;
     })
     .catch(function(error) {
       console.log("error getting menu items: " + error);
+    })
+    .finally(function() {
+      ctrl.loading = false;
     });
   };
 
@@ -68,6 +78,7 @@ function FoundItemsDirective() {
     templateUrl: 'templates/foundItems.html',
     scope: {
       items: '=',
+      loading: '=',
       onRemove: '&removeItem'
     },
     controller: FoundItemsDirectiveController,
@@ -85,8 +96,33 @@ function FoundItemsDirectiveController($scope) {
   itemsCtrl.show_empty_result = false;
 
   $scope.$watch('itemsCtrl.items', function(newValue, oldValue) {
-      itemsCtrl.show_empty_result = oldValue && newValue.length === 0;
+      itemsCtrl.show_empty_result = oldValue && newValue && newValue.length === 0 && !itemsCtrl.loading;
   });
+};
+
+function ItemsLoaderIndicatorDirective() {
+  var ddo = {
+    template: "<div><span><i class='fa fa-spinner fa-spin '></i> Loading...</span></div>",
+    link: link
+  };
+
+  function link(scope, element, attrs) {
+    var ctrl = scope.ctrl;
+
+    function show(isLoading) {
+      if (isLoading)
+        element.css('display', 'block');
+      else {
+        element.css('display', 'none');
+      }
+    };
+
+    scope.$watch('ctrl.loading', function(newValue, oldValue) {
+      show(newValue);
+    });
+  };
+
+  return ddo;
 };
 
 })();
