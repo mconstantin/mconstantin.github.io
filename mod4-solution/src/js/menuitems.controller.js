07d22e12c1351fcffd08d49bb014ac23a0e9a7f1
Michael Constantin <constantinm@sharplabs.com>
2016-10-02 08:50:09 -0700

    added prices

--- mod4-solution/src/js/menuitems.controller.js
+++ mod4-solution/src/js/menuitems.controller.js
@@ -6,7 +6,15 @@
 
   MenuItemsController.$inject = ['menuitems'];
   function MenuItemsController(menuitems) {
-    this.menuitems = menuitems.data.menu_items;
+    this.menuitems = menuitems.data.menu_items.map(function(item) {
+      if (!item.small_portion_name) {
+        item.small_portion_name = "Small";
+      };
+      if (!item.large_portion_name) {
+        item.large_portion_name = "Large";
+      };
+      return item;
+    });
     this.catname = menuitems.data.category.name;
   };
 
